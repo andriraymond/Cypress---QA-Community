4ae3986120a59e09cfac7b507199016e170e7ed6
lalanglalang72 <lalanglalang72@gmail.com>
2024-06-13 12:19:45 +0700

    login issue

--- cypress/e2e/joshua/01. login-saucedemo.cy.js
+++ cypress/e2e/joshua/01. login-saucedemo.cy.js
@@ -1,5 +1,46 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Saucedemo Login Test', () => {
+  it('Login with valid credential', () => {
+    cy.visit('https://www.saucedemo.com/v1/')
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('#login-button').click()
+    cy.url().should('include', '/inventory.html')
+    cy.get('.inventory_list').should('be.visible')
+  })
+
+  it('Login with wrong username', () => {
+    cy.visit('https://www.saucedemo.com/v1/')
+    cy.get('[data-test="username"]').type('invalid_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('#login-button').click()
+    cy.get('[data-test="error"]').should('be.visible')
+  })
+
+  it('Login with wrong passwors', () => {
+    cy.visit('https://www.saucedemo.com/v1/')
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('invalid_password')
+    cy.get('#login-button').click()
+    cy.get('[data-test="error"]').should('be.visible')
+  })
+
+  it('Login with no credential', () => {
+    cy.visit('https://www.saucedemo.com/v1/')
+    cy.get('#login-button').click()
+    cy.get('[data-test="error"]').should('be.visible')
+  })
+
+  it('Login with blank username', () => {
+    cy.visit('https://www.saucedemo.com/v1/')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('#login-button').click()
+    cy.get('[data-test="error"]').should('be.visible')
+  })
+
+  it('Login with blank password', () => {
+    cy.visit('https://www.saucedemo.com/v1/')
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('#login-button').click()
+    cy.get('[data-test="error"]').should('be.visible')
   })
 })
