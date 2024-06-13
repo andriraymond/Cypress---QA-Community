9bba9323e23649dcb464001b22fdbb5537dfafcb
andriraymond <andriraymondo@gmail.com>
2024-06-13 18:21:36 +0700

    create login test case - andri

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  // defaultCommandTimeout: 15000,
+  pageLoadTimeout: 300000,
+  viewportWidth: 1366,
+  viewportHeight: 768,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
