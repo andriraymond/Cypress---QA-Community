0be26b30cac587d48184253f30ab5ff3e1aef9b1
andriraymond <andriraymondo@gmail.com>
2024-06-15 12:23:23 +0700

    update progress

--- cypress/e2e/andri/01. login-saucedemo.cy.js
+++ cypress/e2e/andri/01. login-saucedemo.cy.js
@@ -1,7 +1,9 @@
 describe("01. login-saucedome", () => {
+  // const saucedemo_baseURL = "https://www.saucedemo.com";
+  const saucedemo_baseURL = "https://www.saucedemo.com/v1";
   it("passes to login", () => {
-    cy.visit("https://www.saucedemo.com/v1/"); // redirect to URL saucedemo.com
-    // cy.visit("https://www.saucedemo.com"); // redirect to URL saucedemo.com
+    // cy.visit("https://www.saucedemo.com/v1/"); // redirect to URL saucedemo.com
+    cy.visit(saucedemo_baseURL); // redirect to URL saucedemo.com
 
     const username = "standard_user"; // valid username
     const password = "secret_sauce"; // valid password
@@ -12,16 +14,26 @@ describe("01. login-saucedome", () => {
 
     cy.get("#login-button").click(); // to get the login button and click
     cy.wait(2000); // wait for 2 seconds
-    cy.get(".product_label").should("exist"); // to verify the product label exists
+    // cy.get('//*[@id="header_container"]/div[2]/span').should("exist"); // to verify the product label exists
+    cy.get(".product_label").should("exist"); // to verify the product label exists v1
   });
 
-  it("failed to login", () => {
-    cy.visit("https://www.saucedemo.com/v1/"); // redirect to URL saucedemo.com
-    // cy.visit("https://www.saucedemo.com"); // redirect to URL saucedemo.com
+  it("failed to login - wrong username & password ", () => {
+    cy.visit(saucedemo_baseURL); // redirect to URL saucedemo.com
 
     cy.wait(2000); // wait for 2 seconds
     cy.get('[data-test="username"]').type("wrong_user"); // to get the username field and input the username
     cy.get('[data-test="password"]').type("wrong_password"); // to get the password field and input the password
+
+    cy.wait(2000); // wait for 2 seconds
+    cy.get("#login-button").click(); // to get the login button and click
+    cy.get('[data-test="error"]').should("exist"); // to verify the error message exists
+  });
+
+  it("failed to login - username & password are blank ", () => {
+    cy.visit(saucedemo_baseURL); // redirect to URL saucedemo.com
+
+    cy.wait(2000); // wait for 2 seconds
 
     cy.wait(2000); // wait for 2 seconds
     cy.get("#login-button").click(); // to get the login button and click
